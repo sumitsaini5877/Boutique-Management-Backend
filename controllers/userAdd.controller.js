3b0ba8862ee32c3837657b8259c6936182346da6
sumitsaini5877 <sumitsaini5877@gmail.com>
2024-03-30 21:56:06 +0530

    add user controller start

--- controllers/userAdd.controller.js
+++ controllers/userAdd.controller.js
@@ -9,21 +9,28 @@ const addUser = (req,res)=>{
     var address = req.body.address;
 
 try{
+    if(!name ||!phone || !address){
+        res.status(400).json({
+            status:400,
+            success:false,
+            message:"Fill empty feild"
+        })
+    }else{
 
 
-
-    res.status(200).json({
-        status:200,
-        success:true,
-        data:[
-            {
-                "name":name,
-                "email":email,
-                "phone":phone,
-                "address":address
-            }
-        ]
-    })
+        res.status(200).json({
+            status:200,
+            success:true,
+            data:[
+                {
+                    "name":name,
+                    "email":email,
+                    "phone":phone,
+                    "address":address
+                }
+            ]
+        })
+    }
     
     
 }catch(err){
